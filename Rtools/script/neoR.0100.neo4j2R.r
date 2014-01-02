08f41d47cb9d56a2c4c74b109b9f827b6ed0c2f7
etheleon <picy2k@gmail.com>
2014-01-02 16:02:14 +0800

    neo4j2r acts as a template for querying neo4j (V2.0)

--- Rtools/script/neoR.0100.neo4j2R.r
+++ Rtools/script/neoR.0100.neo4j2R.r
@@ -2,7 +2,17 @@
 library('RCurl')
 library('RJSONIO')
 
-#query FUNCTION
+##################################################
+#--Step1: Set the cypher query 
+#NOTE: Rmbr to escape double quotes accordingly 
+##################################################
+q<-"start read = node:readID('readid:\"HWI-ST884:57:1:1101:13989:75421#0\"') 
+match read-[r1]->gi-->tax-->tax2-[:childof*]->common_ancestor<-[:childof*]-tax3<--gi<-[r2]-read
+where r1.bitscore > \"35\" and r2.bitscore > \"35\" return common_ancestor;"
+
+##################################################
+#--Step2: query FUNCTION
+##################################################
 query <- function(querystring) {
   h = basicTextGatherer()
   curlPerform(url="http://localhost:7474/db/data/cypher",
@@ -19,16 +29,19 @@ sapply(row, function(column) column$data)
   data
 }
 
-q<-"start read = node:readID('readid:\"HWI-ST884:57:1:1101:13989:75421#0\"') 
-match read-[r1]->gi-->tax-->tax2-[:childof*]->common_ancestor<-[:childof*]-tax3<--gi<-[r2]-read
-where r1.bitscore > \"35\" and r2.bitscore > \"35\" return common_ancestor;"
 data<-query(q)
 
-#result list of 2: names(result) [1] columns data
-#1st list: var eg. read & gi
+##################################################
+#Saves the output as a data.frame
+##################################################
+save(data, file="output.rda")
 
-#2nd list: rows
-#result[[2]][[n]] 		where n is the row num 
-  
-#data
-#data[x] where x reps the column
+
+
+##################################################
+#Comments
+#result from curlPerform returns a table in the form of a list, length 2: 
+#[1]names of the columns
+
+#[2]List of rows
+#eg result[[2]][[n]] 		where n is the row num 
