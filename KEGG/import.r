461cd0e8912c3176e40e99b0e05a586210b1a66b
etheleon <picy2k@gmail.com>
2014-01-06 00:15:28 +0800

    fixed the edge output part of KEGG import script

--- KEGG/import.r
+++ KEGG/import.r
@@ -1,67 +1,61 @@
 #!/usr/bin/env Rscript
+
 args=commandArgs(T)
 library(XML)
 
-data=xmlParse(args[1])
-#data=xmlParse("~/kegg_dump/xml/kgml/metabolic/ko/ko00010.xml")
+data=xmlParse(args[1]) #data=xmlParse("~/kegg_dump/xml/kgml/metabolic/ko/ko00010.xml")
 xml_data<-xmlToList(data)
 
-#3 types entry/relation/reaction 
-
-#Pathway information
 pathway.info=setNames(data.frame(t(xml_data[length(xml_data)]$.attrs)), names(xml_data[length(xml_data)]$.attrs))
-#Removes the last row (w/c is the pathway information)
-xml_data=xml_data[-length(xml_data)]
+xml_data=xml_data[-length(xml_data)]  #Removes the last row (w/c is the pathway information)
 
-#KOs associated with rxn
-kos=do.call(rbind, 
-	sapply(xml_data, function(x) { 
+#Types in xml files: "ortholog"     "compound"     "map"          "ECrel"        "maplink"      "reversible"   "irreversible"
+
+#--Orthologs (KOs which work in the same reaction##########################
+ko2rxn=do.call(rbind, sapply(xml_data, function(x) { 
     	    if("graphics" %in% names(x)) { 
-    	    if(x$.attrs[which(names(x$.attrs)=='type')]=='ortholog'){
-	    data.frame(
-		id=as.character(x$.attrs[names(x$.attrs) == 'id']),
-		reaction=as.character(x$.attrs[names(x$.attrs) == 'reaction']),
-		name=as.character(x$.attrs[names(x$.attrs) == 'name'])
-		)
-	    }}}))
+    	    if(x$.attrs[which(names(x$.attrs)=='type')]=='ortholog'){ #there are graphics in the list 
+		reaction=unlist(strsplit(x$.attrs[names(x$.attrs) == 'reaction']," "))
+		name=unlist(strsplit(x$.attrs[names(x$.attrs) == 'name'], " "))
+do.call(rbind,lapply(reaction, function(rxn) { do.call(rbind,lapply(name, function(naa) {data.frame(reaction=rxn,name=naa) }))}))
+				}}}))
+################################################## NOTE single KO can belong to multiple reactions, reactions may include multiple KOs
 
-#Cpds assoc. with reaction
+nodeindex=c("cpd:char:cpdid","ko:char:koid")
+##################################################
+#--EDGES
+edges=do.call(c,lapply(xml_data[which(names(xml_data) %in% "reaction")], function(x) { 
+	    rxnID=unlist(strsplit(x$.attrs[[2]], " "))
+	    rxnTYPE=x$.attrs[[3]] 	#reaction type
 
+	    kos.in.pathway=as.character(subset(ko2rxn, reaction == rxnID)$name) #This line hasnt been PUT USE which KOs are part of a bigger protein complex
+#	    kos.in.pathway=unlist(sapply(kos.in.pathway_complex, function(x) unlist(strsplit(as.character(x), " ")) ))
 
-#Draws the edges
-edges=do.call(c,lapply(xml_data, function(x) { 
-    if("substrate" %in% names(x)) { 
-#the reaction name
-rxnID=x$.attrs[[2]]
-#reaction type
-rxnTYPE=x$.attrs[[3]]
+	    substrates=lapply(which(names(x) == 'substrate'), function(subs) { x[[subs]][[2]]})
+	    products=lapply(which(names(x) == 'product'), function(subs) { x[[subs]][[2]]})
+	    
+	    lapply(list(substrates, products), function(cpd) {  
+	   	do.call(rbind,lapply(cpd, function(s) { 
+    		    do.call(rbind,lapply(kos.in.pathway, function (k) {
+    			    setNames(data.frame(s,k, rxnID, rxnTYPE,stringsAsFactors=F), c(nodeindex[[1]], nodeindex[[2]],"rxnID","rxnDIR"))
+    			    }))		}))		})
+    	    }))
+sub2ko=unique(do.call(rbind,edges[names(edges)=='reaction1']))
+ko2pdt=unique(do.call(rbind,edges[names(edges)=='reaction2']))
 
-#KOs involved in the reaction
-kos.in.pathway_complex=subset(kos, reaction == rxnID)$name
-kos.in.pathway=unlist(sapply(kos.in.pathway, function(x) unlist(strsplit(as.character(x), " ")) ))
+#--Output##################################################
+#Substrate2KO
+sub2koo=cbind(sub2ko, data.frame(relationship='substrateof'))[,c(1,2,5,3)]
+koo2sub=cbind(subset(sub2ko, rxnDIR =='reversible')[,c(2,1,3)], data.frame(relationship='produces'))[,c(1,2,4,3)] #reversible reactions
 
-substrates=sapply(which(names(x) == 'substrate'), function(subs) { x[[subs]][[2]]})
+#KO2Pdt
+koo2pdt=cbind(ko2pdt[,c(2,1,3)] , data.frame(relationship='produces'))[,c(1,2,4,3)]
+pdt2koo=cbind(subset(ko2pdt, rxnDIR =='reversible')[,c(1,2,3)], data.frame(relationship='substrateof'))[,c(1,2,4,3)] #reversible reactions
 
-products=paste(sapply(which(names(x) == 'product'), function(subs) { x[[subs]][[2]]}), collapse=" ")
+#Printing out
 
-#substrate2ko
-sub2ko=do.call(rbind,lapply(substrates, function(s) { 
-    do.call(rbind,lapply(kos.in.pathway, function (k) {
-    	data.frame(rxnID=rxnID, rxnTYPE=rxnTYPE, substrate=as.character(s),ko=as.character(k))
-    }))
-    }))
+##################################################
 
-ko2pdt=do.call(rbind,lapply(products, function(s) { 
-    do.call(rbind,lapply(kos.in.pathway, function (k) {
-    	data.frame(rxnID=rxnID, rxnTYPE=rxnTYPE, product=as.character(s),ko=as.character(k))
-    }))
-    }))
+#--NODES
+#--KOs
 
-list(sub2ko, ko2pdt)
-    }
-}))
-
-sub2ko=do.call(rbind,edges[names(edges)=='reaction1'])
-rownames(sub2ko)<-NULL
-ko2pdt=do.call(rbind,edges[names(edges)=='reaction2'])
-rownames(ko2pdt)<-NULL
